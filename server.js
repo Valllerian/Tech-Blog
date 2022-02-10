bc2391ed8d267393515101147787b1b9d713828c
Valerii Bihun <valeriibihun.co@gmail.com>
2022-02-10 16:09:12 -0700

    workingo n heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+// requiring dependencies;
 const path = require('path')
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+
+// importing helpers;
 const helpers = require('./utils/helpers');
 const routes = require('./controllers');
-require('dotenv').config();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -14,6 +16,7 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
+// setting up cookies and log in timer;
 const sess = {
   secret: 'super secret secret',
   cookie: {maxAge: 86400,},
@@ -26,6 +29,7 @@ const sess = {
 
 app.use(session(sess));
 
+// connecting handlebars;
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -35,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+// listening to the 3001 port;
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
